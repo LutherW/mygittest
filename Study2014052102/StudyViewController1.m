9a11c350f4e5d060587929dc31d28a2ec679336c
tinglu <tinglu860705@163.com>
2014-05-21 09:43:17 +0800

    two files commit!

---
+++ Study2014052102/StudyViewController1.m
@@ -0,0 +1,49 @@
+//
+//  StudyViewController1.m
+//  Study2014052102
+//
+//  Created by TingluWang on 14-5-21.
+//  Copyright (c) 2014年 kaikaidai. All rights reserved.
+//
+
+#import "StudyViewController1.h"
+
+@interface StudyViewController1 ()
+
+@end
+
+@implementation StudyViewController1
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
